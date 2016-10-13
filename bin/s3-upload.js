40402961ae8660f59fe4731910a47639fb935013
nishkixs <nishon.radhakrishnan@gmail.com>
2016-10-13 14:27:34 -0400

    Reinstalled dependecy file type

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -3,25 +3,26 @@
 const fs = require('fs');
 
 const filename = process.argv[2] || '';
+const fileType = require('file-type');
 
 const readFile = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, (error, data)=> {
-      if(error) {
-        reject(error);
-    }
-    resolve(data);
+ return new Promise((resolve, reject) => {
+   fs.readFile(filename, (error, data) => {
+     if (error) {
+       reject(error);
+     }
 
-    });
-  });
+     resolve(data);
+   });
+ });
 };
 
 const logMessage = (data) => {
-  console.log(`${filename} is ${data.length} bytes long`);
+  let typeData = fileType(data);
+ console.log(`${filename} is ${data.length} bytes long ${typeData}`);
 };
 
 readFile(filename)
 .then(logMessage)
-.catch(console.error);
-
-};
+.catch(console.error)
+;
