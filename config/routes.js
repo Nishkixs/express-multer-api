b8d01855a6f5338e74043b8e162a4e23d3c6251a
nishkixs <nishon.radhakrishnan@gmail.com>
2016-10-14 14:51:38 -0400

    Add uploads controller
      use middleware for multer memory config
        do not upload or save a datastore yet.

--- config/routes.js
+++ config/routes.js
@@ -17,5 +17,8 @@ module.exports = require('lib/wiring/routes')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
 
+
+
+.resources('/uploads', { only: ['index', 'show','create'] })
 // all routes created
 ;
